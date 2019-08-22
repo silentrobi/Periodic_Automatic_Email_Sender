ce45212566c2201c9f7bb10576fedc33cd9ec1d4
Mohammad Abu Musa RABIUL <silentrobi840@gmail.com>
2019-08-23 00:55:51 +0300

    Update test.py

--- test.py
+++ test.py
@@ -5,4 +5,4 @@ email = EmailActivity()
 
 email.message("abu.musa.rabiul@gmail.com","silentrobi840@gmail.com","Food request list", 'sending this week\'s food list')
 email.addAttachment("1.csv")
-email.setUpSenderCredential("abu.musa.rabiul@gmail.com",'Acifl1234','smtp.gmail.com',587)
+email.setUpSenderCredential("abu.musa.rabiul@gmail.com",'12345','smtp.gmail.com',587)
