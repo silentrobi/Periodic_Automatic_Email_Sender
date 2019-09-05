e6e0532d36c0302485b5ecc9f8401a08459e6302
silentrobi <silentrobi840@gmail.com>
2019-09-05 21:36:30 +0200

    bug fixed: bug was system used to sent empty csv file.

--- clock.py
+++ clock.py
@@ -23,9 +23,9 @@ def job():
     apiRouteIzmir = apiRouteForBranch('izmir')
     getCurrentWeek = getCurrentDateTime('europe/istanbul').isocalendar()[1]
     getPreviousWeek = getCurrentWeek - 1
-    apiRouteIstanbul = apiRouteIstanbul + str(getPreviousWeek)
+    apiRouteIstanbul = apiRouteIstanbul + str(getCurrentWeek) # Need to change to getPreviousWeek
 
-    apiRouteIzmir = apiRouteIzmir + str(getPreviousWeek)
+    apiRouteIzmir = apiRouteIzmir + str(getCurrentWeek)  # Need to change to getPreviousWeek
     # Get json data from apiroute call
     perser.loadJsonData(apiRouteIstanbul)
     dataIstanbul = perser.getJsonData()
@@ -49,7 +49,7 @@ def job():
     msg = "The attached file has last week's food request list."
     # send email activities
     email = EmailActivity()
-    email.message(senderEmail, "silentrobi840@gmail.com",
+    email.message(senderEmail, "eylul.sert@bordatech.com",
                   "Food request list", msg)
     email.addAttachment("./files/" + fileIstanbul, fileIstanbul)
     email.addAttachment("./files/" + fileIzmir, fileIzmir)
@@ -60,7 +60,7 @@ app = Flask(__name__)
 # BUG(26/08/2019): ValueError: Timezone offset does not match system offset: 10800 != 7200. Please, check your config files.
 # solve specify timezone
 scheduler = BackgroundScheduler(timezone="europe/istanbul")
-scheduler.add_job(func=job, trigger='cron', day_of_week='thu', hour='*', minute='*/2', second='0')
+scheduler.add_job(func=job, trigger='cron', day_of_week='fri', hour='9', minute='0', second='0')
 # Explicitly kick off the background thread
 scheduler.start()
 
